f957192250ec72499f12f5e26c047e5370052a5a
Suleman <slmn95@rediffmail.com>
2025-07-23 21:01:47 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-./home/ilfs/Public/whook/.venv/bin/python/gunicorn --worker-class eventlet -w 1 --bind 0.0.0.0:5000 app:app
+uv run python worker.py &
+gunicorn --worker-class eventlet -w 1 --bind 0.0.0.0:5000 app:app &
